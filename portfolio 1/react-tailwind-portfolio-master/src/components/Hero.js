2a2e50d413ae56c774f084476bd5e9001698d923
Kishorekanna27 <97933492+Kishorekanna27@users.noreply.github.com>
2024-02-27 05:56:40 +0530

    Update Hero.js

--- portfolio 1/react-tailwind-portfolio-master/src/components/Hero.js
+++ portfolio 1/react-tailwind-portfolio-master/src/components/Hero.js
@@ -5,9 +5,9 @@ export default function Hero() {
     const config  = {
         subtitle: 'Im a Full-stack developer and Designer',
         social: {
-            twitter: 'https://twitter.com/jvlcode',
-            facebook: 'https://facebook.com/jvlcode',
-            linkedin: 'https://in.linkedin.com/company/jvl-code'
+            twitter: 'https://twitter.com/kishore',
+            facebook: 'https://facebook.com/kishorekanna',
+            linkedin: 'https://www.linkedin.com/in/kishore-kanna-226403222/'
         }
     }
 
